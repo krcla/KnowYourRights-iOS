31d98f9a410c8dc0cebcc8c730ad042ce30a7629
zuwhan <JL1217jl>
2018-03-10 11:28:30 -0800

    Fixed an issue with the speaker. Changed it to a button (instead of a web-link). Set the version number.

--- KnowYourRights/ToOfficersViewController.swift
+++ KnowYourRights/ToOfficersViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class ToOfficersViewController: UIViewController, UIWebViewDelegate {
     @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var speakButton: UIButton!
+
     let player = AVQueuePlayer()
     
     override func viewDidLoad() {
@@ -20,8 +22,9 @@ class ToOfficersViewController: UIViewController, UIWebViewDelegate {
         let requestUrl = URL(fileURLWithPath: filePath!)
         let myRequest = URLRequest(url: requestUrl)
         webView.loadRequest(myRequest)
-        webView.delegate = self
         self.view.addSubview(webView)
+
+        speakButton.addTarget(self, action: #selector(speakButtonClicked), for: .touchUpInside)
     }
     
     override func didReceiveMemoryWarning() {
@@ -29,38 +32,15 @@ class ToOfficersViewController: UIViewController, UIWebViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
-        switch navigationType {
-        case .linkClicked:
-            guard let url = request.url else { return true }
-            if (url.lastPathComponent == "to_officers.html") {
-                let queryItems = URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems
-                let speak = (queryItems?.first(where: { $0.name == "speak" })?.value)!
-                if (speak == "true") {
-                    if (player.rate != 0 && player.error == nil) {
-                        player.pause()
-                    } else {
-                        let filePath = Bundle.main.path(forResource: "to_officers", ofType: "m4a", inDirectory: "Resources.bundle")
-                        let url = URL(fileURLWithPath: filePath!)
-                        player.removeAllItems()
-                        player.insert(AVPlayerItem(url: url), after: nil)
-                        player.play()
-                    }
-                    return false
-                }
-            }
-            
-            // Open links in Safari.
-            if #available(iOS 10.0, *) {
-                UIApplication.shared.open(url, options: [:], completionHandler: nil)
-            } else {
-                // openURL(_:) is deprecated in iOS 10+.
-                UIApplication.shared.openURL(url)
-            }
-            return false
-        default:
-            // Handle other navigation types.
-            return true
+    @objc func speakButtonClicked(_ sender: UIButton) {
+        if (player.rate != 0 && player.error == nil) {
+            player.pause()
+        } else {
+            let filePath = Bundle.main.path(forResource: "to_officers", ofType: "m4a", inDirectory: "Resources.bundle")
+            let url = URL(fileURLWithPath: filePath!)
+            player.removeAllItems()
+            player.insert(AVPlayerItem(url: url), after: nil)
+            player.play()
         }
     }
 }
